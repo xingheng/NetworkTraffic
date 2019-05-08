b38e72f8fbc281fdf1697f9e031629c1d3bceedf
Wei Han <xingheng907@hotmail.com>
2019-05-08 16:52:30 +0800

    Build the reserved today widget extension controller.

---
+++ NetworkTrafficUsage/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  NetworkTrafficUsage
+//
+//  Created by WeiHan on 2019/5/8.
+//  Copyright © 2019 WillHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
