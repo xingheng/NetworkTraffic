db2acde0c8a1343c2286c98cf131f8892fdaae16
Wei Han <xingheng907@hotmail.com>
2019-05-08 15:27:50 +0800

    Initial Commit

---
+++ NetworkTraffic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetworkTraffic
+//
+//  Created by WeiHan on 2019/5/8.
+//  Copyright © 2019 WillHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
